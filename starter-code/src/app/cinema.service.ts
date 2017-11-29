5edca664259a4b738bbbf15c2fd04bd87c52fab4
Tess <tessdegregorio@gmail.com>
2017-11-29 19:01:15 +0100

    iteration 1 done

---
+++ starter-code/src/app/cinema.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import movieSample from 'sample-movies';
+
+
+@Injectable()
+export class CinemaService {
+  movies = [];
+
+constructor () {}
+
+getMovies(){
+
+}
+
+getMovie(id){
+
+}
+}
